8f265bf5fb4ba9fd8083263cb871aa3e8eaca331
begi <siukaevi_giga@yahoo.com>
2025-06-02 16:52:33 +0400

    job's done

---
+++ mainFour.js
@@ -0,0 +1,8 @@
+let number = 0;
+for (i=0; i<1000; i++){
+    if (i % 3 == 0 || i % 5 == 0) {
+        number = number + i
+    }
+    
+};
+console.log (number)
---
+++ mainOne.js
@@ -0,0 +1,17 @@
+let number = 0
+for (let i=0; i<100; i++){
+    number++
+
+    if (number%3 == 0 && number%5 == 0) {
+        console.log("FizzBuzz")
+        
+    } else if (number%3 == 0) {
+        console.log("Fizz")
+    }
+    else if (number%5 == 0) {
+        console.log("Buzz")
+    }
+    else {
+        console.log(number)
+    }
+}
